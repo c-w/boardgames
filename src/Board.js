e33fe356a81f794d30294613e8aa3a89fad4922d
Clemens Wolff <clewolff@microsoft.com>
2020-04-17 18:55:09 -0400

    Display card help text

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import { last, sum } from './utils';
 
+const CARD_TEXTS = {
+  1: 'If you play this and lose the trick, you lead the next trick.',
+  3: 'When you play this, you may exchange the trump card with a card from your hand.',
+  5: 'When you play this, draw 1 card. Then discard any 1 card to the bottom of the deck (face down).',
+  7: 'The winner of the trick receives 1 point for each 7 in the trick.',
+  9: 'When determining the winner of a trick with only one 9, treat the 9 as if it were in the trump suit.',
+  11: 'When you lead this, if your opponent has a card of this suit, they must play either the 1 of this suit or their highest ranked card of this suit.',
+};
+
 function Card({ rank, suit, onClick, enabled }) {
   if (!rank || !suit) {
     return null;
@@ -60,6 +69,7 @@ export default function Board({ G, ctx, playerID, moves }) {
   const tricksWon = G.tricks.filter(t => t.winner === playerID).length;
   const tricksLost = G.tricks.filter(t => t.winner !== playerID).length;
   const lastTrick = G.tricks.length >= 1 ? last(G.tricks) : null;
+  const helpText = chosen.length > 0 ? CARD_TEXTS[player.hand[chosen[0]].rank] : null;
 
   const hand = player.hand.map((card, i) => ({ card, i })).sort((a, b) => {
     return a.card.suit === b.card.suit
@@ -129,6 +139,11 @@ export default function Board({ G, ctx, playerID, moves }) {
         disabled={!isActive || chosen.length === 0}
         value={isDiscard ? 'Discard card' : 'Play card'}
       />
+      {helpText && (
+        <aside>
+          {helpText}
+        </aside>
+      )}
     </form>
   );
 }
