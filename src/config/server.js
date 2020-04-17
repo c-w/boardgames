7486e0ae7921370cef4254b9232cfa6192fe6e5f
Clemens Wolff <clewolff@microsoft.com>
2020-04-17 19:15:15 -0400

    Add persistence to server

--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,6 @@
 import envalid from 'envalid';
 
 export default envalid.cleanEnv(process.env, {
+  STATE_DIR: envalid.str(),
   PORT: envalid.port(),
 });
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import { Server } from 'boardgame.io/server';
+import { FlatFile, Server } from 'boardgame.io/server';
 import config from './config/server';
 import Game from './game';
 
@@ -6,6 +6,10 @@ const server = Server({
   games: [
     Game
   ],
+
+  db: new FlatFile({
+    dir: config.STATE_DIR,
+  }),
 });
 
 server.run(config.PORT, () => console.log(`Server running on port ${config.PORT}`));
