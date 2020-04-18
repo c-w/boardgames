34307667afa9723ea2f023338a6d3b546a1082f6
Clemens Wolff <clewolff@microsoft.com>
2020-04-17 23:57:51 -0400

    Enable serving of frontend in server

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,4 +1,6 @@
 import { FlatFile, Server } from 'boardgame.io/server';
+import serve from 'koa-static';
+import path from 'path';
 import config from './config';
 import Game from '../shared/game';
 
@@ -11,5 +13,7 @@ const server = Server({
     dir: config.STATE_DIR,
   }),
 });
+
+server.app.use(serve(path.join(__dirname, '..', '..', 'build')));
 
 server.run(config.PORT, () => console.log(`Server running on port ${config.PORT}`));
