de0f30d7a4ff3e9d3590e0f49603f2dc310bd1e2
Clemens Wolff <clewolff@microsoft.com>
2020-10-11 20:32:07 -0400

    Remove unused constant

--- src/frontend/config.js
+++ src/frontend/config.js
@@ -2,6 +2,5 @@ import envalid from 'envalid';
 
 export default envalid.cleanEnv(process.env, {
   REACT_APP_WAITING_FOR_PLAYER_REFRESH_MS: envalid.num({ default: 2000 }),
-  REACT_APP_ROOM_LIST_REFRESH_MS: envalid.num({ default: 5000 }),
   REACT_APP_SERVER_URL: envalid.url({ default: window.location.origin }),
 });
