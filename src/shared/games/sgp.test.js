9ea60d9f1d606445b7582549e713a02408aa9a41
Clemens Wolff <clewolff@microsoft.com>
2020-10-19 15:13:59 -0400

    Add tests for maki

--- src/shared/games/sgp.test.js
+++ src/shared/games/sgp.test.js
@@ -1,4 +1,5 @@
 import { scoreCard } from './sgp';
+import { removeAt } from '../utils';
 
 describe('Score Nigiri', () => {
   test.each([
@@ -10,5 +11,99 @@ describe('Score Nigiri', () => {
     const otherHands = [];
     const round = 1;
     expect(scoreCard(hand[0], hand, otherHands, round)).toEqual(expected);
+  });
+});
+
+describe('Score Maki Rolls', () => {
+  test('4 players', () => {
+    const hands = [
+      [
+        { name: 'Maki', category: 'Rolls', count: 3 },
+        { name: 'Maki', category: 'Rolls', count: 2 },
+      ],
+      [
+        { name: 'Maki', category: 'Rolls', count: 2 },
+        { name: 'Maki', category: 'Rolls', count: 1 },
+      ],
+      [
+        { name: 'Maki', category: 'Rolls', count: 2 },
+        { name: 'Egg', category: 'Nigiri' },
+      ],
+      [
+        { name: 'Maki', category: 'Rolls', count: 1 },
+        { name: 'Egg', category: 'Nigiri' },
+      ],
+    ];
+
+    const round = 1;
+
+    expect(scoreCard(hands[0][0], hands[0], removeAt(hands, 0), round)).toEqual(6);
+    expect(scoreCard(hands[1][0], hands[1], removeAt(hands, 1), round)).toEqual(3);
+    expect(scoreCard(hands[2][0], hands[2], removeAt(hands, 2), round)).toEqual(0);
+    expect(scoreCard(hands[3][0], hands[3], removeAt(hands, 3), round)).toEqual(0);
+  });
+
+  test('4 players, tied', () => {
+    const hands = [
+      [
+        { name: 'Maki', category: 'Rolls', count: 3 },
+        { name: 'Maki', category: 'Rolls', count: 2 },
+      ],
+      [
+        { name: 'Maki', category: 'Rolls', count: 3 },
+        { name: 'Maki', category: 'Rolls', count: 2 },
+      ],
+      [
+        { name: 'Maki', category: 'Rolls', count: 2 },
+        { name: 'Egg', category: 'Nigiri' },
+      ],
+      [
+        { name: 'Maki', category: 'Rolls', count: 1 },
+        { name: 'Egg', category: 'Nigiri' },
+      ],
+    ];
+
+    const round = 1;
+
+    expect(scoreCard(hands[0][0], hands[0], removeAt(hands, 0), round)).toEqual(6);
+    expect(scoreCard(hands[1][0], hands[1], removeAt(hands, 1), round)).toEqual(6);
+    expect(scoreCard(hands[2][0], hands[2], removeAt(hands, 2), round)).toEqual(3);
+    expect(scoreCard(hands[3][0], hands[3], removeAt(hands, 3), round)).toEqual(0);
+  });
+
+  test('6 players', () => {
+    const hands = [
+      [
+        { name: 'Maki', category: 'Rolls', count: 3 },
+        { name: 'Maki', category: 'Rolls', count: 2 },
+      ],
+      [
+        { name: 'Maki', category: 'Rolls', count: 2 },
+        { name: 'Maki', category: 'Rolls', count: 1 },
+      ],
+      [
+        { name: 'Maki', category: 'Rolls', count: 2 },
+        { name: 'Egg', category: 'Nigiri' },
+      ],
+      [
+        { name: 'Maki', category: 'Rolls', count: 1 },
+        { name: 'Egg', category: 'Nigiri' },
+      ],
+      [
+        { name: 'Egg', category: 'Nigiri' },
+        { name: 'Egg', category: 'Nigiri' },
+      ],
+      [
+        { name: 'Egg', category: 'Nigiri' },
+        { name: 'Egg', category: 'Nigiri' },
+      ],
+    ];
+
+    const round = 1;
+
+    expect(scoreCard(hands[0][0], hands[0], removeAt(hands, 0), round)).toEqual(6);
+    expect(scoreCard(hands[1][0], hands[1], removeAt(hands, 1), round)).toEqual(4);
+    expect(scoreCard(hands[2][0], hands[2], removeAt(hands, 2), round)).toEqual(2);
+    expect(scoreCard(hands[3][0], hands[3], removeAt(hands, 3), round)).toEqual(0);
   });
 });
