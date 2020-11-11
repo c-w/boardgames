d07abb806bcf73ced9884ac4d338434992b5397c
Clemens Wolff <clewolff@microsoft.com>
2020-11-11 13:11:10 -0500

    Exclude CSS of other games from bundle

--- src/index.js
+++ src/index.js
@@ -5,22 +5,24 @@ import './index.scss';
 
 /**
  * @param {string} gameName
- * @returns {any[]}
+ * @returns {Promise<any>[]}
  */
 function importGame(gameName) {
   switch (gameName) {
     case 'the-vole-in-the-valley':
       return [
-        () => import('./shared/games/fitf'),
-        () => import('./frontend/boards/fitf/rules.md'),
-        () => import('./frontend/boards/fitf'),
+        import('./shared/games/fitf'),
+        import('./frontend/boards/fitf/rules.md'),
+        import('./frontend/boards/fitf'),
+        import('./frontend/boards/fitf/index.scss'),
       ];
 
     case 'sashimi-express':
       return [
-        () => import('./shared/games/sgp'),
-        () => import('./frontend/boards/sgp/rules.md'),
-        () => import('./frontend/boards/sgp'),
+        import('./shared/games/sgp'),
+        import('./frontend/boards/sgp/rules.md'),
+        import('./frontend/boards/sgp'),
+        import('./frontend/boards/sgp/index.scss'),
       ];
 
     default:
@@ -28,20 +30,13 @@ function importGame(gameName) {
   }
 }
 
-const [
-  gameImporter,
-  rulesImporter,
-  boardImporter,
-] = importGame(process.env.REACT_APP_BUILD_TARGET);
+const importers = importGame(process.env.REACT_APP_BUILD_TARGET);
 
-Promise.all([
-  boardImporter(),
-  gameImporter(),
-  rulesImporter(),
-]).then(([
-  boardModule,
+Promise.all(importers).then(([
   gameModule,
   rulesModule,
+  boardModule,
+  _styleModule,
 ]) => {
   ReactDOM.render(
     <StrictMode>
