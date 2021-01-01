ea99cdda1af9217a5fa770942cb5914dc835e8d2
c-w <c-w@users.noreply.github.com>
2021-01-01 22:58:25 +0000

    deploy: 571dee79c7a02789236f21928d18067930678ac6

--- sashimi-express/static/js/10.93618a33.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpboardgames=this.webpackJsonpboardgames||[]).push([[10],{594:function(s,a,o){}}]);
-//# sourceMappingURL=10.93618a33.chunk.js.map
--- sashimi-express/static/js/9.fd8d4357.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpboardgames=this.webpackJsonpboardgames||[]).push([[9],{591:function(s,a,o){}}]);
-//# sourceMappingURL=9.fd8d4357.chunk.js.map
